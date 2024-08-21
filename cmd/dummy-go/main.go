95ac94dd6ef8902a878fa25cc62b7940454bdad0
HBAQQAL <hamzabaqqal2002@gmail.com>
2024-08-21 18:28:51 +0100

    add some sleep

--- cmd/dummy-go/main.go
+++ cmd/dummy-go/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("dummy go started")
 
 	for {
-
+		time.Sleep(10 * time.Second)
+		fmt.Println("dummy go running")
 	}
 }
