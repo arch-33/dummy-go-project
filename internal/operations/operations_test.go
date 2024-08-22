46f422b930b96c6a6188f34f4e49398f2b2cd730
HBAQQAL <hamzabaqqal2002@gmail.com>
2024-08-22 16:06:27 +0100

    fix faillure

--- internal/operations/operations_test.go
+++ internal/operations/operations_test.go
@@ -15,7 +15,7 @@ func TestSum(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	result := Subtract(5, 3)
-	expected := 5
+	expected := 2
 
 	if result != expected {
 		t.Errorf("Subtract(5, 3) = %d; expected %d", result, expected)
